6d9ad554e7820608327287ffc217dda7c98febef
Naous <103106889+Naawa@users.noreply.github.com>
2025-02-03 16:02:20 -0800

    Update data.ts

--- src/routes/timer/components/data.ts
+++ src/routes/timer/components/data.ts
@@ -28,9 +28,9 @@ function getTimeBlocks(weekday: boolean, hour: number) {
 }
 
 export const firstHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 10) : getTimeBlocks(true, 15)
-export const secondHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 11) : getTimeBlocks(false, 16)
-export const thirdHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 12) : getTimeBlocks(false, 17)
-export const lastHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 13) : getTimeBlocks(false, 18)
+export const secondHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 11) : getTimeBlocks(true, 16)
+export const thirdHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 12) : getTimeBlocks(true, 17)
+export const lastHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 13) : getTimeBlocks(true, 18)
 
 export const activities: Activity[] = [
     {
