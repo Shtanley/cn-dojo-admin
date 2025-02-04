a637d2645f4152c256dc5f05cc22afce016a5cce
Naous <103106889+Naawa@users.noreply.github.com>
2025-02-03 16:26:41 -0800

    Update data.ts

--- src/routes/timer/components/data.ts
+++ src/routes/timer/components/data.ts
@@ -18,7 +18,7 @@ function getTimeBlocks(weekday: boolean, hour: number) {
         activityTimeBlock.setHours(hour, minute, 0, 0)
         timeBlocks.push(activityTimeBlock)
 
-        if(i == 2) {
+        if(i == 1) {
           minute += 20
          }
          else { 
