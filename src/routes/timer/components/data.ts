eb3f70b9839217de92fc142c3fb177d721d0f821
Naawa <naous.islam@outlook.com>
2025-02-05 13:37:44 -0800

    yuh

--- src/routes/timer/components/data.ts
+++ src/routes/timer/components/data.ts
@@ -28,8 +28,8 @@ function getTimeBlocks(weekday: boolean, hour: number) {
     return timeBlocks
 }
 
-export const firstHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 15) : getTimeBlocks(true, 12)
-export const secondHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 16) : getTimeBlocks(true, 13)
+export const firstHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 15) : getTimeBlocks(true, 15)
+export const secondHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 16) : getTimeBlocks(true, 16)
 export const thirdHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 17) : getTimeBlocks(true, 17)
 export const lastHourTimeBlocks = new Date().getDay() == 6 ? getTimeBlocks(false, 18) : getTimeBlocks(true, 18)
 
